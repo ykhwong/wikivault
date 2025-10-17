e296499839cde0d44f687f935ab5610cbf9fe017
ted <ykhwong@daum.net>
2025-10-17 14:47:37 +0900

    Use default version of gemini-2.5-flash

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -12,7 +12,7 @@ const CONFIG = {
       : [],
     GEMINI_URL:
       "https://generativelanguage.googleapis.com/v1beta/models/" +
-      "gemini-2.5-flash-preview-05-20:streamGenerateContent",
+      "gemini-2.5-flash:streamGenerateContent",
     WIKI_URL: "https://en.wikipedia.org/w/api.php"
   },
   CORS: {
@@ -76,5 +76,6 @@ const CONFIG = {
     API_CREATE: 'api_create.log'
   }
 };
+
 
 module.exports = CONFIG; 
